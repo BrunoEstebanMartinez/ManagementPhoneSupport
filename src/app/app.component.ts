1a2253d7b175c2526d79906ea17b85d88d2d4027
Bruno Martínez <bruno.esteban.mtz.millan@gmail.com>
2022-04-14 23:41:07 -0500

    v1.1.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'managementphonesfix';
 
-  ngOnInit(){
-    this.getPhones();
-  }
-
-  getPhones(){
-
-  }
-    
-  
-}
+export class AppComponent implements OnInit{ngOnInit(){}}
